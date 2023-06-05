9d2329c96e7f41603deee4a7f8c709239a169ed6
Liliya Belkova <work_ft@protonmail.com>
2023-06-05 21:10:43 +0400

    feat: made slider

---
+++ slider-two/main.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container');
+const boxColors = document.querySelector('.colors');
+const boxImages = document.querySelector('.images');
+const image = boxImages.querySelectorAll('div');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+
+let activeSlideIndex = 0;
+
+upButton.addEventListener('click', () => {
+    activeSlideIndex++
+        if (activeSlideIndex === image.length) {
+            activeSlideIndex = 0;
+        }
+        rollSlider();
+});
+
+downButton.addEventListener('click', () => {
+    activeSlideIndex--;
+        if (activeSlideIndex <= 0) {
+            activeSlideIndex = image.length -1;
+        }
+        rollSlider();
+});
+
+const height = container.clientHeight;
+console.log(height)
+
+function rollSlider() {
+    boxImages.style.transform = 'translateY(-' + activeSlideIndex * height + 'px)';
+    boxColors.style.transform = `translateY(${activeSlideIndex * height}px)`
+    console.log(boxColors.style.transform)
+    console.log(boxImages.style.transform)
+}
+rollSlider();
