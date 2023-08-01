5ee886cf51f6e8e15b92bc922c404b4b30c3a41f
Liliya Belkova <work_ft@protonmail.com>
2023-08-01 20:00:02 +0400

    fix: fixed function togglerActiveClasses

--- CSS-Accordion/main.js
+++ CSS-Accordion/main.js
@@ -2,34 +2,19 @@ const listItem = Array.from(document.querySelectorAll('.list-item'));
 const buttons = document.querySelectorAll('.list-item__button');
 const listImg = document.querySelectorAll('.list-image');
 
-console.log(listItem);
-
-
 
 function togglerActiveClasses() {
     for (let i = 0; i < listItem.length; i++) {
         if (listItem[i]) {
             listItem[i].addEventListener('click', () => {
-                listItem[i].classList.add('active');
+                listItem[i].classList.toggle('active');
                 if (listItem[i].classList.contains('active')) {
                     buttons[i].classList.add('active');
                     listImg[i].style.maxHeight = listImg[i].scrollHeight + 'px';
-                    listImg[i].style.overflow = 'visible';
-                    listImg[i].style.transition = 'max-height 0.4s';
-                };
+                    listImg[i].style.transition = 'max-height 0.6s';
+                } else listImg[i].style.maxHeight = 0;
             });
         }
     };
 };
 togglerActiveClasses();
-
-function clearActiveClasses() {
-    for (let i = 0; i < listItem.length; i++) {
-        if (listItem[i].classList.contains('active')) {
-            listItem[i].addEventListener('click', () => {
-                listItem[i].classList.remove('active');
-            });
-        };
-    }
-};
-clearActiveClasses();
